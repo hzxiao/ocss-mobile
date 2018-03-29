082ad18c2d6ee4f64922d4b6508f9eb723b6d675
Xiaozh <838223935@qq.com>
2018-03-29 23:32:00 +0800

    init project

---
+++ ocss-mobile/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace ocssmobile
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
